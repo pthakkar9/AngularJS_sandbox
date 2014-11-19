f84d7b5b34c44064a532a8c82585053822a42d1c
Parva Thakkar <parva.thakkar@gmail.com>
2014-11-18 23:56:58 -0500

    adding filters

--- app.js
+++ app.js
@@ -8,8 +8,12 @@
   });
 
   var gems = [
-    { name: 'Azurite', price: 2.95 },
-    { name: 'Bloodstone', price: 5.95 },
-    { name: 'Zircon', price: 3.95 },
+    { name: 'Azurite', price: 2.95, soldOut: false },
+    { name: 'Bloodstone', price: 5.95, soldOut: true },
+    { name: 'Zircon', price: 3.95, soldOut: false },
+    { name: 'Amber', price: 1, soldOut: false },
+    { name: 'Citrine', price: 21.3, soldOut: true },
+    { name: 'Coral', price: 2.99, soldOut: false },
+    { name: 'Topaz', price: 11.34, soldOut: false },
   ];
 })();
