a6cca70f8cdd87057720eb2af60859c8ac9acc2f
Parva Thakkar <parva.thakkar@gmail.com>
2014-11-19 20:36:36 -0500

    images, tabs etc.

--- app.js
+++ app.js
@@ -7,13 +7,47 @@
     
   });
 
+  app.controller('TabController', function(){
+
+  	// Sets default tab to first tab
+  	this.tab = 1;
+
+  	this.setTab = function(inTab){
+  		this.tab = inTab;
+  	};
+
+  	this.isSet = function(inTab){
+  		return this.tab === inTab;
+  	};
+  	
+  });
+
+   app.controller('GalleryController', function(){
+
+    this.current = 0;
+
+    this.setCurrent = function(newGallery){
+      this.current = newGallery || 0;
+    };
+
+  });
+
   var gems = [
-    { name: 'Azurite', price: 2.95, soldOut: false },
-    { name: 'Bloodstone', price: 5.95, soldOut: true },
-    { name: 'Zircon', price: 3.95, soldOut: false },
-    { name: 'Amber', price: 1, soldOut: false },
-    { name: 'Citrine', price: 21.3, soldOut: true },
-    { name: 'Coral', price: 2.99, soldOut: false },
-    { name: 'Topaz', price: 11.34, soldOut: false },
+    { name: 'Azurite', price: 2.95, soldOut: false, 
+    description: "description comes here", 
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"]},
+    { name: 'Bloodstone', price: 5.95, soldOut: true, description: "description comes here", 
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"]},
+    { name: 'Zircon', price: 3.95, soldOut: false, description: "description comes here", 
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
+    { name: 'Amber', price: 1, soldOut: false, description: "description comes here", 
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
+    { name: 'Citrine', price: 21.3, soldOut: true, description: "description comes here", 
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
+    { name: 'Coral', price: 2.99, soldOut: false, description: "description comes here", 
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
+    { name: 'Topaz', price: 11.34, soldOut: false, description: "description comes here", 
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
   ];
+
 })();
