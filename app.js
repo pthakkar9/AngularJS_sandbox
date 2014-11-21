d173c8b843a764eb4a28af966f9350e4b1d9e04d
Parva Thakkar <parva.thakkar@gmail.com>
2014-11-20 22:37:23 -0500

    adding dynamic reviews with ng-model

--- app.js
+++ app.js
@@ -33,21 +33,52 @@
   });
 
   var gems = [
-    { name: 'Azurite', price: 2.95, soldOut: false, 
-    description: "description comes here", 
-    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"]},
+    { name: 'Azurite', price: 2.95, soldOut: false, description: "description comes here", 
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"], reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "This gem sucks.",
+        author: "tim@example.org",
+        createdOn: 1397490980837
+      }]},
+    
     { name: 'Bloodstone', price: 5.95, soldOut: true, description: "description comes here", 
     images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"]},
+    
     { name: 'Zircon', price: 3.95, soldOut: false, description: "description comes here", 
-    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"], reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 1,
+        body: "This gem sucks.",
+        author: "tim@example.org",
+        createdOn: 1397490980837
+      }] },
+    
     { name: 'Amber', price: 1, soldOut: false, description: "description comes here", 
     images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
+    
     { name: 'Citrine', price: 21.3, soldOut: true, description: "description comes here", 
     images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
+    
     { name: 'Coral', price: 2.99, soldOut: false, description: "description comes here", 
     images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
+    
     { name: 'Topaz', price: 11.34, soldOut: false, description: "description comes here", 
-    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"] },
+    images: ["images/gem-02.gif", "images/gem-05.gif", "images/gem-09.gif"], reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org",
+        createdOn: 1397490980837
+      }] 
+  },
   ];
 
 })();
